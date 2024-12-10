c4d378610f2f010224275d9d66eef26c09fe5e0b
Golden-flames21 <99491777+Golden-flames21@users.noreply.github.com>
2024-12-10 16:02:09 +0000

    added features

---
+++ src/utils/useFetch.jsx
@@ -0,0 +1,22 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+export const useFetch = (url) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        setIsLoading(true);
+        const res = await fetch(url);
+        const result = await res.json();
+        setIsLoading(false);
+        setData(result);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getData();
+  }, [url]);
+  return { data, isLoading };
+};
